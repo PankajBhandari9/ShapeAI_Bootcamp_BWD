54e2c6eb0bcd26103dfb4b7d6151429b60bfe343
PankajBhandari9 <79698858+PankajBhandari9@users.noreply.github.com>
2021-06-05 13:04:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+import "../public/styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  rootElement
+);
